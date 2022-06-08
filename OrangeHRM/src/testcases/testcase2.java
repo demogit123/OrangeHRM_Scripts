a86ccba04e0e115727e8be7c2597fcca0189bda7
anjalinandekar <106538725+anjalinandekar@users.noreply.github.com>
2022-06-08 17:47:45 +0530

    Update testcase2.java

--- OrangeHRM/src/testcases/testcase2.java
+++ OrangeHRM/src/testcases/testcase2.java
@@ -2,6 +2,7 @@ package testcases;
 
 public class testcase2 {
 
+	//test case 2
 	public static void main(String[] args) {
 		System.out.println("This is Testcase2");
 		System.out.println("Testcase2 updated....");
